ebf9c1c41cb7553714cfc260804fa32ee74810fa
ttejas16 <jagtaptejas128@gmail.com>
2024-05-03 13:59:51 +0530

    minor change(2)

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "",
+    baseURL: "https://sttacked-backend.onrender.com/api/v1/",
     headers: {
         "Content-Type": "application/json"
     },
--- src/api/socketConfig.js
+++ src/api/socketConfig.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("", {
+const socket = io("https://sttacked-backend.onrender.com", {
     autoConnect: false,
     withCredentials: true
 });
